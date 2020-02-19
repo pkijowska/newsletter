907ce25502d57bc6b12ff85ba2a07bb902027b20
Paulina <p.kijowska@gmail.com>
2020-02-19 17:36:14 +1100

    signup works

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 Made a sign up form using bootstrap and express and connected it to the mailchimp's server  
 
-:boom: Express/
-:boom: Body Parser/
-:boom: Javascript/
-:boom: Bootstrap/
+:boom: Express\
+:boom: Body Parser\
+:boom: Javascript\
+:boom: Bootstrap\
+:boom: Mailchimp API\
--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const request = require("request");
+const request = require("request");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
 
 app.listen(3000, function(req, res) {
   console.log("server is running on port 3000");
@@ -17,5 +21,48 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-  console.log(req.body.name,  req.body.lastname, req.body.email);
+  var firstname = req.body.name;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+
+  var data ={
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstname,
+        LNAME: lastname
+      }
+    }]
+  }
+
+  var jsonData = JSON.stringify(data);
+  console.log(jsonData);
+  var options = {
+    url: "https://us4.api.mailchimp.com/3.0/lists/e1bb5cc754",
+    method: "POST",
+    headers: {
+      "Authorization": "paula1 444d098d88f23d36c17e357b84ac4fcb-us4"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body){
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    }else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      }else {
+        res.send("there was an error with signing up, please try again")
+      }
+    }
+  });
+
+
+
 });
+
+
+// 444d098d88f23d36c17e357b84ac4fcb-us4
+// e1bb5cc754
